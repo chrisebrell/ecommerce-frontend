20ac831d7da606287309f615d91b163ccc0b0aec
Bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2024-03-22 21:45:51 -0400

    adding products list component

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,16 +1,34 @@
+import { Link } from 'react-router-dom'
 import '../styles/productCard.style.scss'
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product
+    // shortening of name 
+    let formatName = ''
+    if (name.length > 20) {
+        const splitName = name.split(' ')
+        for (let i = 0; i < splitName.length; i++) {
+            if (formatName.length < 20) {
+                formatName += splitName[i] + ' '
+            }
+        }
+    } else {
+        formatName = name
+    }
+    console.log(formatName);
     return (
         <>
             <div className="product-card-container">
                <img className='product-image' src={imageUrl}  alt={`${name}`} />
                <div className="product-detail">
-                   <span>{name}</span> 
+                   {/* <span>{name.substr(0, 30)}</span>  */}
+                   <span>{formatName}</span> 
                    <span> ${price}</span> 
                </div>
-               <button className='button-container'>Add to Cart</button>
+               {/* <div className='buttons-container'> */}
+                {/* <button className='button-container'>ADD TO CART</button> */}
+                <Link to='/description' state={{ product: product}}><button className='button-container'>VIEW</button></Link>
+               {/* </div> */}
             </div>
         </>
     )
