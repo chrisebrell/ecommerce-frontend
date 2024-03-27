fb22fd2105eda3fdafacc8c41d7a54fccbfd7da5
Bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2024-03-27 00:07:34 -0400

    creating product context

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,51 +1,23 @@
-import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { ProductsContext } from '../context/products.context';
 import '../styles/categories.style.scss'
 
 const Categories = () => {
-    const categories = [
-        {
-          "id": 1,
-          "title": "Home & Living",
-          "imageUrl": "https://images.unsplash.com/photo-1513161455079-7dc1de15ef3e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9tZSUyMGRlY29yfGVufDB8fDB8fHww"
-        },
-        {
-          "id": 2,
-          "title": "Kitchen",
-          "imageUrl": "https://images.unsplash.com/photo-1556912173-46c336c7fd55?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGtpdGNoZW58ZW58MHx8MHx8fDA%3D"
-        },
-        {
-          "id": 3,
-          "title": "Women's Clothing",
-          "imageUrl": "https://images.unsplash.com/photo-1516762689617-e1cffcef479d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xvdGhpbmd8ZW58MHx8MHx8fDA%3D"
-        },
-        {
-          "id": 4,
-          "title": "Electronics & Accessories",
-          "imageUrl": "https://images.unsplash.com/photo-1593344484962-796055d4a3a4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGVsZWN0cm9uaWNzfGVufDB8fDB8fHww"
-        },
-        {
-          "id": 5,
-          "title": "Art & Collectibles",
-          "imageUrl": "https://images.unsplash.com/photo-1695142258314-180ea86bdb48?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFydCUyMGNvbGxlY3RpYmxlc3xlbnwwfHwwfHx8MA%3D%3D"
-        },
-        {
-          "id": 6,
-          "title": "Men's Clothing",
-          "imageUrl": "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVucyUyMGNsb3RoaW5nfGVufDB8fDB8fHww"
-        }
-      ]
-
+  const {categories} = useContext(ProductsContext); 
       return (
         <div className="categories-container">
             {
                 categories.map((category) => {
                     return (
-                    <div key={category.id} className="category-container">
+                    <div key={category._id} className="category-container">
                         <div className='background-image' style={{backgroundImage: `url(${category.imageUrl})`}}  />
-                        <div className='category-body-container'>
-                            <h2>{category.title}</h2>
-                            <p>Shop Now</p>
-                        </div>    
+                          <div className='category-body-container'>
+                            <Link to='/products' state={{category: category._id}}>
+                                <h2>{category.name}</h2>
+                                <p>Shop Now</p>
+                            </Link>    
+                          </div>
                     </div>
                     )
                 })
--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,15 +1,46 @@
-import { createContext, useState } from "react";
-
-import ProductsData from '../products.json';
+import { createContext, useState, useEffect } from "react";
 
 export const ProductsContext = createContext({
-    products: []
+    products: [],
+    categories: []
 });
 
 export const ProductProvider = ({children}) => {
-    const [products, setProducts] = useState(ProductsData);
+    const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () => {
+        const fetchData = async () => {
+            const url = process.env.REACT_APP_API_CATEGORIES_URL  || `http://localhost:3000/api/categories`
+            // console.log(url);
+            const response = await fetch(url)
+            const data = await response.json()
+            if (data.length) {
+                setCategories(data)
+                // console.log(data)
+            }
+        }
+        fetchData()
+      }, []) 
+
+      useEffect( () => {
+        const fetchData = async () => {
+            const url = process.env.REACT_APP_API_BASE_URL || `http://localhost:3000/api/products` 
+            const response = await fetch(url)
+            const data = await response.json()
+            if (data.length) {
+                setProducts(data)
+            }
+        }
+        fetchData()
+    }, [])
+
+    const value = {
+        products, 
+        categories
+    }
     return (
-        <ProductsContext.Provider value={products}>{children}</ProductsContext.Provider>
+        <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
     )
 }
 
