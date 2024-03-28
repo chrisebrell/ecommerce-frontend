f45d780a513974dcc0ab3a7ae8a260eb8c5da817
Bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2024-03-27 20:22:38 -0400

    creating new route for products based on category

---
+++ src/components/categoryProducts.jsx
@@ -0,0 +1,32 @@
+import { useState, useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { ProductsContext } from "../context/products.context";
+import ProductCard from "./productCard";
+import '../styles/products.style.scss'
+
+const CategoryProducts = () => {
+const {products} = useContext(ProductsContext)
+const location = useLocation();
+let category = null
+if (location.state) {
+    category = location.state.category
+}
+let productsData = products
+// filter products using category._id passed thru useLocation
+if (category) {
+    productsData = products.filter(product => product.category === category)
+} 
+    return (
+        <div className="products-container">
+            {
+                productsData.map((product) => (
+                    <div key={product._id}>
+                        <ProductCard product={product} />
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default CategoryProducts;
